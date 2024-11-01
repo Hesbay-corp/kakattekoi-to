46ce3f980c398fb3bd9aa0c18399522082b812c9
GitHub Action <action@github.com>
2024-11-01 21:52:05 +0000

    Automatic build

--- assets/en-B32ZkEJL.js
+++
@@ -1,1 +0,0 @@
-const t={name:"Kakatte Koi.gg"},e={toast:{"error-title":"{nb, plural, =1 {Erreur} other {Erreurs}}"}},o={app:t,component:e};export{t as app,e as component,o as default};
--- assets/fr-Q21KBba9.js
+++
@@ -1,1 +0,0 @@
-const t={name:"Kakatte Koi.gg"},r={toast:{"error-title":"{nb, plural, =1 {Erreur} other {Erreurs}}"}},o={app:t,component:r};export{t as app,r as component,o as default};
